1456703b846591177461d576f56d1a1a7fff26e0
Ben Biederman <benbiederman@gmail.com>
2024-05-31 08:44:33 -0500

    Footer development.
    Changes include:
    - Add social media icons for use in footer
    - Refactor main.js for consistency in building items.
    - Add elements for footer in main.js
    - Move filter and fetch data into helpers file
    - Add styling for footer
    - Update verbiage for some headings on index.html

---
+++ js/helpers/filterData.js
@@ -0,0 +1,21 @@
+export const sortByTagsAndRating = (data) => {
+  const priority = {
+    Coffee: 1,
+    Breakfast: 2,
+    Lunch: 3,
+  };
+
+  return data.sort((a, b) => {
+    const aTagPriority = priority[a.tags[0]] || 99;
+    const bTagPriority = priority[b.tags[0]] || 99;
+
+    if (aTagPriority < bTagPriority) return -1;
+    if (aTagPriority > bTagPriority) return 1;
+
+    // If both have the same tag priority, sort by rating
+    if (a.rating > b.rating) return -1;
+    if (a.rating < b.rating) return 1;
+
+    return 0;
+  });
+};
